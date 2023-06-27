73e3533dee4c69e2371208bc23620d0a08b48635
Alex Chiew <concussion239@gmail.com>
2023-06-27 14:27:02 +0800

    Add getOrderById function to retrieve orders from DynamoDB

---
+++ src/api/functions/get-single-order/app.js
@@ -0,0 +1,55 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+
+const tableName = process.env.ORDER_TABLE;
+
+if (!tableName) {
+    throw new Error("The ORDER_TABLE environment variable is not set");
+}
+
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+export const getOrderById = async (event) => {
+    if (event.httpMethod !== "GET") {
+        return {
+            statusCode: 400,
+            body: `getOrderById only accept GET method, you tried: ${event.httpMethod}`,
+        };
+    }
+
+    const orderId = event.pathParameters.orderId;
+
+    if (!orderId) {
+        return {
+            statusCode: 400,
+            body: "Missing orderId",
+        };
+    }
+
+    const get_item = {
+        user_id: "static_user",
+        id: orderId,
+    };
+
+    const params = {
+        TableName: tableName,
+        Key: get_item,
+    };
+
+    const command = new GetCommand(params);
+
+    try {
+        const data = await ddbDocClient.send(command);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data.Item),
+        };
+    } catch (err) {
+        console.error("Failure", err.message);
+        return {
+            statusCode: 500,
+            body: "Failed to retrieve the order",
+        };
+    }
+};
